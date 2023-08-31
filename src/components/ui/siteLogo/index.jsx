2c4662a9eaeb4ad67c69b042e8e494d417052d69
Juan <juanfelipecardenas4@gmail.com>
2023-08-30 20:06:36 -0400

    Added: logo navigation

--- src/components/ui/siteLogo/index.jsx
+++ src/components/ui/siteLogo/index.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import sailBoat from "../../../assets/logos/sailboat.svg";
 
 import "./styles.scss";
 const SiteLogo = ({ hasText = true }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+  };
   return (
-    <span className="site-logo">
+    <span className="site-logo" onClick={handleClick}>
       <span className="site-logo__background">
         <img src={sailBoat} className="site-logo__icon" />
       </span>
